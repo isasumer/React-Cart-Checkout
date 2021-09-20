efd4bc940a08f818bad15540510a077ebf8836c9
Isa SUMER <sumerisa5@gmail.com>
2021-09-20 17:40:16 +0300

    data js created

---
+++ redux/src/components/Products.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { sepeteEkle } from "../actions/action";
+const Products = (props) => {
+  return (
+    <div>
+      <h2>
+        <span>Kitap Listesi</span>
+        <Link to="/cart">Sepetim</Link>
+      </h2>
+
+      {props.bookList.map((book) => (
+        <div className="book">
+          <img src={book.image} alt={book.name} />
+          <div>
+            <h4>{book.name}</h4>
+            <p>Yazar: {book.author}</p>
+            <p>Fiyat: &#8378;{book.price}</p>
+            <button onClick={()=> props.sepeteEkle(book)}>Sepete Ekle</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    bookList: state.bookList,
+    cart:state.cart
+  };
+};
+
+export default connect(mapStateToProps, { sepeteEkle })(Products);
--- redux/src/index.js
+++ redux/src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
+import { reducer } from "./reducers/reducer";
+import { Provider } from "react-redux";
+
+const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 reportWebVitals();
