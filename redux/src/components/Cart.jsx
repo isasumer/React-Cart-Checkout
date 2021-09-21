4849dd5d2f3263df2b4514b465b5d51f3c297199
Isa SUMER <sumerisa5@gmail.com>
2021-09-21 10:49:59 +0300

    action jsd created

--- redux/src/components/Cart.jsx
+++ redux/src/components/Cart.jsx
@@ -1,46 +1,40 @@
 import React from "react";
-import {  BrowserRouter as Router,
-    Link} from "react-router-dom";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 import { connect } from "react-redux";
 
-
-
 const Cart = (props) => {
-  const totalPrice = props.cart.reduce((total, item) => (total += item.price), 0)
+  const totalPrice = props.cart.reduce(
+    (total, item) => (total += item.price), 0);
   return (
-      <Router>
-    <div>
-      <h2>
-        <Link to="/">Kitap Listesi</Link> <span>Sepetim</span>
-      </h2>
+    <Router>
+      <div>
+        <h2>
+          <Link to="/">Kitap Listesi</Link> <span>Sepetim</span>
+        </h2>
 
-      <h3>Toplam Sepet Tutarı: &#8378;{totalPrice.toFixed(2)}</h3>
-{props.cart.map((book) => (
-  <div className="book">
-        <img
-          src={book.image}
-          alt={book.name}
-        />
-        <div>
-          <h4>{book.name}</h4>
-          <p>Yazar: {book.author}</p>
-          <p>Fiyat: &#8378;{book.price}</p>
-          <button>-</button>
-          <button>Sepetten Çıkar</button>
-          <button>+</button>
-        </div>
+        <h3>Toplam Sepet Tutarı: &#8378;{totalPrice.toFixed(2)}</h3>
+        {props.cart.map((book) => (
+          <div className="book">
+            <img src={book.image} alt={book.name} />
+            <div>
+              <h4>{book.name}</h4>
+              <p>Yazar: {book.author}</p>
+              <p>Fiyat: &#8378;{book.price}</p>
+              <button>-</button>
+              <button>Sepetten Çıkar</button>
+              <button>+</button>
+            </div>
+          </div>
+        ))}
       </div>
-))}
-      
-    </div>
     </Router>
   );
 };
 
 const mapStateToProps = (state) => {
   return {
-    cart:state.cart
-  }
-}
+    cart: state.cart,
+  };
+};
 
 export default connect(mapStateToProps)(Cart);
